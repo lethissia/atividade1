9c3637faac7064215065fe9a07eb3dc4423ddb05
Lethíssia Carvalho Sousa <lethissia@alu.ufc.br>
2021-10-08 17:32:03 -0300

    Initial commit

---
+++ atividade1/components/Aluno.jsx
@@ -0,0 +1,15 @@
+import React, {Component} from 'react'
+import {Text,View} from 'react-native'
+
+function Aluno(){
+    return(
+        <View>
+            <Text style={{color:'pink', fontWeight:'bold'}}>Lethíssia Carvalho Sousa</Text>
+            <Text style={{color:'pink', fontWeight:'bold'}}>21 anos</Text>
+            <Text style={{color:'pink', fontWeight:'bold'}}>Fortaleza, Ceará</Text>
+            <Text style={{color:'pink', fontWeight:'bold'}}>Design Digital</Text>
+        </View>
+    )
+}
+
+export default Aluno
